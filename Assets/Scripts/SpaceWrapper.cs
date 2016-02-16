979d4c75eaa167fca2f501e0558fbe9568dd9591
Ben Scott <ias.jazz@gmail.com>
2016-02-16 14:51:49 -0500

    removed rotate tile accidentally

--- Assets/Scripts/SpaceWrapper.cs
+++ Assets/Scripts/SpaceWrapper.cs
@@ -37,20 +37,20 @@ public class SpaceWrapper : MonoBehaviour, ISpace<Tiles> {
 
     public Quaternion TargetRotation {get;set;}
 
-	public Dir Direction {
-		get { return CurrentSpace.Direction; }
-		set { if (CurrentSpace==null) return;
-			CurrentSpace.Direction = value;
+    public Dir Direction {
+        get { return CurrentSpace.Direction; }
+        set { if (CurrentSpace==null) return;
+            CurrentSpace.Direction = value;
             TargetRotation = Quaternion.Euler(0f,(float) value,0f);
-		}
-	}
+        }
+    }
 
-	public bool HasWater {
-		get { return (CurrentSpace!=null && CurrentSpace.HasWater); }
-		set { if (CurrentSpace==null) return;
-			CurrentSpace.HasWater = value;
-		}
-	}
+    public bool HasWater {
+        get { return (CurrentSpace!=null && CurrentSpace.HasWater); }
+        set { if (CurrentSpace==null) return;
+            CurrentSpace.HasWater = value;
+        }
+    }
 
     public bool IsEmpty {
         get { return (CurrentSpace!=null && CurrentSpace.IsEmpty); } }
@@ -73,45 +73,48 @@ public class SpaceWrapper : MonoBehaviour, ISpace<Tiles> {
     public IEnumerator MakingMove() {
         if (wait) yield break;
         wait = true;
-		MakeMove();
+        MakeMove();
         yield return new WaitForSeconds(0.05f);
         wait = false;
     }
 
-	public void MakeMove() {
-		Tiles oldTileVal = CurrentSpace.Value;
-		CurrentSpace.Value = IconSelector.Current;
-		if (board.board.IsBoardValid()) {
-			if (oldTileVal == CurrentSpace.Value) {
-				Direction = (Dir)(((int)Direction+90)%360);
-				//Debug.Log ("Dir changed! new dir is:");
-				//Debug.Log (CurrentSpace.Direction);
-				//var rotation = new Vector3(0,(float) -90,0);
-				//CurrentTile.transform.Rotate(rotation);
-			} else {
-				var newTile = IconSelector.CreateTile(IconSelector.Current);
-				CurrentTile = newTile;
-				//CurrentSpace.Direction = IconSelector.CurrentSelectDir;
-				newTile.transform.parent = this.transform;
-				newTile.transform.localPosition = Vector3.zero;
-			}
+    public void MakeMove() {
+        Tiles oldTileVal = CurrentSpace.Value;
+        CurrentSpace.Value = IconSelector.Current;
+        if (board.board.IsBoardValid()) {
+            if (oldTileVal == CurrentSpace.Value) {
+                RotateTile();
+                //Debug.Log ("Dir changed! new dir is:");
+                //Debug.Log (CurrentSpace.Direction);
+                //var rotation = new Vector3(0,(float) -90,0);
+                //CurrentTile.transform.Rotate(rotation);
+            } else {
+                var newTile = IconSelector.CreateTile(IconSelector.Current);
+                CurrentTile = newTile;
+                //CurrentSpace.Direction = IconSelector.CurrentSelectDir;
+                newTile.transform.parent = this.transform;
+                newTile.transform.localPosition = Vector3.zero;
+            }
 
-			board.board.UpdateWater();
+            board.board.UpdateWater();
             if (clip)
                 GetComponent<AudioSource>().PlayOneShot(clip);
-		} else {
+        } else {
             if (failClip)
                 GetComponent<AudioSource>().PlayOneShot(failClip,0.7f);
-			CurrentSpace.Value = oldTileVal;
-		}
-	}
+            CurrentSpace.Value = oldTileVal;
+        }
+    }
+
+    public void RotateTile() {
+        Direction = (Dir)(((int)Direction+90)%360); }
 
 
     public void OnMouseOver() {
-		if (Input.GetButtonUp ("Fire1")) MakeMove();
+        if (Input.GetButtonUp ("Fire1")) MakeMove();
 //        while (!wait) {
-//			if (Input.GetButtonUp("Fire1"))
-//				yield return StartCoroutine(MakingMove());
+//          if (Input.GetButtonUp("Fire1"))
+//              yield return StartCoroutine(MakingMove());
 //            else yield return new WaitForEndOfFrame();
 //        }
     }
