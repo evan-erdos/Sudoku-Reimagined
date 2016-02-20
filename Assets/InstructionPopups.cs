bafe47117c668d720672a3c272210abecd8ee56c
Francisco Rojo <frojo@andrew.cmu.edu>
2016-02-20 15:28:10 -0500

    Tutorial instruction sequence working

--- Assets/InstructionPopups.cs
+++ Assets/InstructionPopups.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class InstructionPopups : MonoBehaviour {
 
+	int currentInstrScreen = 1;
+
 	// Use this for initialization
 	void Start () {
-	
+		transform.Find ("Instr1").gameObject.SetActive (true);
+	}
+
+	public void NextInstructionScren() {
+		string currInstrName = "Instr" + currentInstrScreen.ToString ();
+		this.currentInstrScreen++;
+		string nextInstrName = "Instr" + currentInstrScreen.ToString ();
+
+		Transform nextInstr = transform.Find (nextInstrName);
+			if (nextInstr)
+				nextInstr.gameObject.SetActive (true);
+		transform.Find (currInstrName).gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
